89d3542ea242b751f1d392058890c5890d2fabfc
AyushWaghe <ayushwaghe23@gmail.com>
2026-04-15 22:40:06 +0530

    Basic saving of transaction completed.Implementing exception and contraint handling

---
+++ src/main/java/org/example/dao/TransactionRepository.java
@@ -0,0 +1,10 @@
+package org.example.dao;
+
+import org.example.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction,Integer> {
+
+}
