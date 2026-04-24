4346dd03b0e541e343f0fea201c1b2d71cae089c
AyushWaghe <ayushwaghe23@gmail.com>
2026-04-24 18:39:51 +0530

    Saving transactions along with category-Completed

--- src/main/java/org/example/services/TransactionService.java
+++ src/main/java/org/example/services/TransactionService.java
@@ -1,16 +1,21 @@
 package org.example.services;
 
+import org.example.dao.CategoryRepository;
 import org.example.dao.TransactionRepository;
 import org.example.dao.UserRepository;
 import org.example.dto.TransactionRequest;
 import org.example.exceptions.UserDetailNotFoundException;
+import org.example.models.Category;
 import org.example.models.Transaction;
 import org.example.models.User;
+import org.example.utils.CategoryUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionService {
@@ -21,26 +26,38 @@ public class TransactionService {
     @Autowired
     UserRepository userRepository;
 
-    public Transaction saveTransaction(TransactionRequest transactionRequest){
+    @Autowired
+    CategoryRepository categoryRepository;
 
-        if(transactionRequest.getAmount().compareTo(BigDecimal.ZERO)<=0) {
-            throw new IllegalArgumentException("Transaction amount must be greater then 0");
-        }
+    @Autowired
+    CategoryService categoryService;
+
+    public Transaction saveTransaction(TransactionRequest transactionRequest){
 
         User user = userRepository.findById(transactionRequest.getUserId())
                 .orElseThrow(() -> new UserDetailNotFoundException("User with id "+transactionRequest.getUserId()+" not found"));
+
+        Category category=categoryService.findOrCreateCategory(user,transactionRequest.getCategory());
+
         Transaction transaction= Transaction.builder()
                 .user(user)
                 .title(transactionRequest.getTitle())
                 .description(transactionRequest.getDescription())
                 .amount(transactionRequest.getAmount())
-                .category(transactionRequest.getCategory())
+                .category(category)
                 .transactionDate(transactionRequest.getTransactionDate())
                 .type(transactionRequest.getType())
                 .spendingType(transactionRequest.getSpendingType())
                 .build();
 
         return transactionRepository.save(transaction);
+    }
 
+    public List<Transaction> getTransactions(Integer userId,String month) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserDetailNotFoundException("User with id "+userId+" not found"));
+
+        List<Transaction> transactions;
+        
     }
 }
