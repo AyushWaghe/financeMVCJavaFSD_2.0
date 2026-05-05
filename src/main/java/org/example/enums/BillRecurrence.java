7707f83268bb159ba197b651169180160ad49745
AyushWaghe <ayushwaghe23@gmail.com>
2026-05-05 17:02:13 +0530

    Bill schema

---
+++ src/main/java/org/example/enums/BillRecurrence.java
@@ -0,0 +1,10 @@
+package org.example.enums;
+
+public enum BillRecurrence {
+    DAILY,
+    WEEKLY,
+    MONTHLY,
+    QUATERLY,
+    HALF_YEARLY,
+    YEARLY
+}
---
+++ src/main/java/org/example/enums/BillStatus.java
@@ -0,0 +1,7 @@
+package org.example.enums;
+
+public enum BillStatus {
+    PAID,
+    OVERDUE,
+    PENDING
+}
